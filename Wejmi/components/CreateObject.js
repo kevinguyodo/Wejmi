50f416e48d2b527ba1830021d92dc968b9a3da9d
Gracieux <60341447+Gracieux31@users.noreply.github.com>
2022-03-24 13:16:43 +0100

    create cards

--- Wejmi/components/CreateObject.js
+++ Wejmi/components/CreateObject.js
@@ -1,9 +1,69 @@
-import { Text, View } from "react-native";
+import React, { useState } from "react";
+import { TextInput, SafeAreaView, StyleSheet, Text, View, Button } from "react-native";
+import { Card } from 'react-native-paper';
 
-export default () => {
+const App = () => {
+  const [text, setText,] = useState('');
   return (
-    <View>
-      <Text>Details</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.container}>
+        <Card>
+      <Card style={styles.card} >
+          <Text style={styles.paragraph}>
+          circle picture
+          </Text>
+        </Card>
+
+        <Card>
+          <Text style={styles.paragraph}>
+          title text
+          </Text>
+        </Card>
+
+        <Card>
+        <TextInput
+        style={{width: 150, height: 150}}
+        placeholder=" Description de l'ajout!"
+        onChangeText={newText => setText(newText)}
+        defaultValue={text}
+      />
+        </Card>
+
+        </Card>
+      < Button style={styles.buttonHome} title="Valider" />
+
+      </View>
+    </SafeAreaView>
   );
 };
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 20,
+    backgroundColor: '#ecf0f1',
+  },
+  paragraph: {
+   // flex :2,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    padding: 20
+  },
+  card: {
+    //flex: 1,
+  }
+});
+
+
+// export default () => {
+
+//   return (
+//     <View >
+//       <Text>Cards</Text>
+//     </View>
+//   );
+// };
--- Wejmi/components/Home.js
+++ Wejmi/components/Home.js
@@ -1,20 +1,17 @@
 import { Image,Text, View, Button, StyleSheet } from "react-native";
 import logo from '../assets/logo.png'; 
 
-const viewObjectDetails = () => {
-  navigation;
-};
 
 export default ({ navigation }) => {
   const viewDetails = () => {
-    navigation.navigate("Details");
+    navigation.navigate("Create");
     console.log("Click");
   };
 
   return (
     <View style={styles.containerHome}>
      <Image source={logo} style={{ width: 305, height: 159 }} /> 
-      <Text style={styles.home}>Bienvenue sur l'app de RANGEMENT</Text>
+      {/* <Text style={styles.home}>Bienvenue sur l'app de RANGEMENT</Text> */}
       <Button style={styles.buttonHome} title="Accéder a la bibliotheque" onPress={viewDetails}>
       </Button>
     </View>
